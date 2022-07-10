4f88d8b98f95b41bf28f892bdd84c3446ae99995
MichaelIanCzech <108399306+MichaelIanCzech@users.noreply.github.com>
2022-07-10 22:42:49 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+Projekt może być udostępniany i wykorzystywany pod warunkiem spełnienia poniższych punktów:
+1. Elementy lub cały projekt nie będzie wykorzystany w celach komercyjnych lub zarobkowych
+2. Wyraźnie zostanie dodana informacja o autorach wraz z linkiem do repozytorium na github.com
+
+***
+
+Project might be published and used only under following conditions:
+1. Non commercian and non profit use only
+2. Clearly added information about authors with the link to the github.com repository
---
+++ parkic.js
@@ -0,0 +1,484 @@
+var w = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {"id":"1", "nazwa":"Park Stanisława Moniuszki", "adres":"Centrum między ulicami Libelta, Noskowskiego, Chopina i Al. Niepodległości","data":"1905","pow":"2,2 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.918451786041256,
+          52.41339479584031
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"id":"2", "nazwa":"Park Adama Mickiewicza", "adres":"Fredry 7","data":"1910","pow":"3,54 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.91691756248474,
+          52.40896417697235
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"id":"3", "nazwa":"Park Stare Koryto Warty", "adres":"koryto rzeki na Chwaliszewie","data":"bd","pow":"3 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.942827701568604,
+          52.40748502343099
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"id":"4", "nazwa":"Park Henryka Wieniawskiego", "adres":"Wieniawskiego","data":"1910","pow":"2,68 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.91622018814087,
+          52.41071815317217
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"id":"5", "nazwa":"Park Karola Marcinkowskiego", "adres":"centrum przy alei Niepoldegłości i ulicy Towarowej","data":"1905","pow":"9,4 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.91767930984497,
+          52.404631295035315
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"id":"6", "nazwa":"Park Fryderyka Chopina", "adres":"Centrum","data":"koniec XVII w.","pow":"1,8 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.93494737148285,
+          52.40532183569461
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"id":"7", "nazwa":"Park Jana Henryka Dąbrowskiego", "adres":"Ogrodowa","data":"1832","pow":"4 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.926627159118652,
+          52.40298508105129
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"id":"8", "nazwa":"Park Tadeusza Mazowieckiego", "adres":"Bolesława Krzywoustego","data":"2014","pow":"7 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.939029693603516,
+          52.39963027992144
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"id":"9", "nazwa":"Park Romana Maciejewskiego", "adres":"Centrum","data":"2012","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.91738426685333,
+          52.40618581447237
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"id":"10", "nazwa":"Ogród Zamkowy im. Ofiar Katynia i Sybiru", "adres":"Święty Marcin 80/82","data":"1910","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.91867172718048,
+          52.40899362864099
+        ]
+      }
+    },
+	{
+		"type":"Feature",
+		"properties":{"id":"43", "nazwa":"Park na osiedlu Oświecenia", "adres":"Oświecenia","data":"1997","pow":"8,41 ha"},
+		"geometry":{"type":"Point","coordinates":[
+		16.963813304901123,52.38957416751841
+		]
+		}
+		},
+		
+		{"type":"Feature",
+		"properties":{"id":"42", "nazwa":"Park na osiedlu Bolesława Chrobrego", "adres":"Bolesława Chrobrego","data":"1986","pow":"bd"},
+		"geometry":{"type":"Point","coordinates":[
+		16.91449284553528,52.45560404501683
+		]
+		}
+		},
+		
+		{
+      "type": "Feature",
+      "properties": {"id":"11", "nazwa":"Park Thomasa Woodrow Wilsona", "adres":"Śniadeckich 30","data":"1901","pow":"7,2 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.901439179386177,52.40029589819792 
+		  ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"12", "nazwa":"Park Cytadela", "adres":"aleja Armii Poznań","data":"1970","pow":"100 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.935697738182636, 52.42211867862429
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"13", "nazwa":"Park Tysiąclecia", "adres":"między Warszawską a Jeziorem Maltańskim","data":"1936","pow":"26,3 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.96796754196658,52.407684835932415
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"14", "nazwa":"Park Karola Kurpińskiego", "adres":"między Serafitek a Świętego Rocha","data":"1938","pow":"mniej niż 1 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.947926717761963,52.39914896489351
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"15", "nazwa":"Park ks. Tadeusza Kirschke", "adres":"Nadolnik","data":"2014","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.96896972569733,52.4248289620035
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"16", "nazwa":"Park Astronomiczny Jana Heweliusza", "adres":"skrzyżowanie ulic Heweliusza, Palacza i Księżycowej","data":"2017","pow":"4 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.873709198712813,52.396463393110814
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"17", "nazwa":"Park Szelągowski", "adres":"Szelągowska 41","data":"bd","pow":"7,8 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.94990004104152,52.424064533373226
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"18", "nazwa":"Park Adama Wodziczki", "adres":"pomiędzy al. Wielkopolską a doliną Bogdanki","data":"1972","pow":"7 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.91236606987743,52.42000790933544
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"19", "nazwa":"Park Gustawa Manitiusa", "adres":"pomiędzy Grunwaldzką i Reymonta","data":"1952","pow":"6 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.887318581516368,52.40137616927794
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"20", "nazwa":"Park Drwęskich", "adres":"Niedziałkowskiego 2a","data":"1954","pow":"4,54 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.923516554532636,52.400025272080704
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"21", "nazwa":"Park Świętego Jana Pawła II", "adres":"Dolna Wilda 64","data":"1970","pow":"20 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.927435241040527,52.38958066596532
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"22", "nazwa":"Park Maltański", "adres":"wokół Jeziora Maltańskiego","data":"bd","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.95548157358198,52.4068552895345
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"23", "nazwa":"Park Rataje", "adres":"pomiędzy osiedlami Rataje i Żegrze","data":"2018","pow":"14,67 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+         16.956197641040372,52.38474873456401
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"24", "nazwa":"Park Jana Kasprowicza", "adres":"pomiędzy Reymonta, Wyspiańskiego, Jarochowskiego i Chociszewskiego","data":"1939","pow":"9,3 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+         16.877818041040282,52.38196552861135
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"25", "nazwa":"Park nad Wartą", "adres":"os. Piastowskie","data":"lata 50/60 XX w.","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.940682727548708,52.38957333671191
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"24", "nazwa":"Park Jana Kasprowicza", "adres":"pomiędzy Reymonta, Wyspiańskiego, Jarochowskiego i Chociszewskiego","data":"1939","pow":"9,3 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+           16.891387639188306,52.3954712226933
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"27", "nazwa":"Park na os. Armii Krajowej", "adres":"Armii Krajowej","data":"bd","pow":"5,45 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.955791227550353, 52.380919993987355
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"28", "nazwa":"Park. ks. Józefa Jasińskiego", "adres":"os. Grunwald Południe","data":"2005","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.868623925696184,52.38526561814422
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"29", "nazwa":"Park Kosynierów", "adres":"Aleja pod Lipami","data":"lata 60 XX w.","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.946230841041707,52.42944890513398
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"30", "nazwa":"Park ks. Feliksa Michalskiego", "adres":"Grunwaldzka 272","data":"bd","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+         16.846536414056786,52.38668332791245
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"31", "nazwa":"Park Międzyosiedlowy", "adres":"pomiędzy osiedlami Batorego a Chrobry","data":"bd","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.922690312206417,52.45568395557007
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"32", "nazwa":"Park Antoninek", "adres":"Browarna 3","data":"lata 70 XX w.","pow":"2,1 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.02530056987707,52.40713383470455
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"33", "nazwa":"Park Bambrów Poznańskich", "adres":"Orzechowa 1","data":"2013","pow":"2,6 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.911940198711942,52.36517213372898
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"34", "nazwa":"Park Edukacji Ekologicznej Strzeszyn", "adres":"Strzeszyn Górecki","data":"2013","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.883434556386295,52.4426666594325
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"35", "nazwa":"Park Forteczny", "adres":"Chartowo","data":"bd","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.986096098712473,52.38451878602117
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"36", "nazwa":"Park Sołacki", "adres":"Sołacz","data":"1911","pow":"14,63 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.897769541041487,52.423514134435024
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"37", "nazwa":"Park Podolany im. prof. Leszka Begera", "adres":"Strzeszyńska 79/81","data":"2013","pow":"9,3 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.890644969878302,52.44987117980107
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"38", "nazwa":"Park Władysława Czarneckiego", "adres":"Przyjaźni","data":"lata 90 XX w.","pow":"5 ha"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.929259998713956,52.434024208615895
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"39", "nazwa":"Park Jurija Gagarina", "adres":"Pod Lipami","data":"1976","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.940858998713843,52.43339146218436
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"40", "nazwa":"Park na osiedlu Zwycięstwa", "adres":"Zwycięstwa","data":"lata 90 XX w.","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.925581967722973,52.43984960936113
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"41", "nazwa":"Park przy osiedlu Botanika", "adres":"Bolesława Śmiałego 34","data":"bd","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.92195490812502,52.447484957220574
+
+        ]
+      }
+    },
+	{
+	"type": "Feature",
+      "properties": {"id":"44", "nazwa":"Park na osiedlu Jana III Sobieskiego", "adres":"Bolesława Śmiałego 34","data":"bd","pow":"bd"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.91110392208145,52.46423354119602
+
+        ]
+      }
+    }
+	
+  ]
+}
